b4551910ddb8035b5c8f9d8b7ec39d4290ddd1e3
Destio Wahyu <destiowahyu@gmail.com>
2022-07-10 16:50:14 +0000

    Update ci_shop/application/language/index.html, ci_shop/application/libraries/index.html, ci_shop/application/logs/index.html, ci_shop/application/models/Cart_model.php, ci_shop/application/models/Category_model.php, ci_shop/application/models/Checkout_model.php, ci_shop/application/models/Home_model.php, ci_shop/application/models/index.html, ci_shop/application/models/Login_model.php, ci_shop/application/models/Myorder_model.php, ci_shop/application/models/Order_model.php, ci_shop/application/models/Product_model.php, ci_shop/application/models/Profile_model.php, ci_shop/application/models/Register_model.php, ci_shop/application/models/Shop_model.php, ci_shop/application/models/User_model.php, ci_shop/application/third_party/index.html, ci_shop/application/views/welcome_message.php, ci_shop/application/views/index.html, ci_shop/application/views/errors/cli/error_404.php, ci_shop/application/views/errors/cli/error_db.php, ci_shop/application/views/errors/cli/error_general.php, ci_shop/application/views/errors/cli/error_exception.php, ci_shop/application/views/errors/cli/index.html, ci_shop/application/views/errors/cli/error_php.php, ci_shop/application/views/errors/html/error_404.php, ci_shop/application/views/errors/html/error_db.php, ci_shop/application/views/errors/html/error_exception.php, ci_shop/application/views/errors/html/error_general.php, ci_shop/application/views/errors/html/error_php.php, ci_shop/application/views/errors/html/index.html, ci_shop/application/views/errors/index.html, ci_shop/application/views/layouts/_alert.php, ci_shop/application/views/layouts/_navbar.php, ci_shop/application/views/layouts/_menu.php, ci_shop/application/views/layouts/_status.php, ci_shop/application/views/layouts/app.php, ci_shop/application/views/pages/auth/register.php, ci_shop/application/views/pages/auth/login.php, ci_shop/application/views/pages/cart/index.php, ci_shop/application/views/pages/category/form.php, ci_shop/application/views/pages/category/index.php, ci_shop/application/views/pages/checkout/index.php, ci_shop/application/views/pages/checkout/success.php, ci_shop/application/views/pages/home/index.php, ci_shop/application/views/pages/myorder/confirm.php, ci_shop/application/views/pages/myorder/detail.php, ci_shop/application/views/pages/myorder/index.php, ci_shop/application/views/pages/order/detail.php, ci_shop/application/views/pages/order/index.php, ci_shop/application/views/pages/product/form.php, ci_shop/application/views/pages/product/index.php, ci_shop/application/views/pages/profile/index.php, ci_shop/application/views/pages/profile/form.php, ci_shop/application/views/pages/user/form.php, ci_shop/application/views/pages/user/index.php

---
+++ ci_shop/application/views/pages/checkout/success.php
@@ -0,0 +1,32 @@
+<main role="main" class="container">
+
+    <div class="row">
+        <div class="col-md-12">
+            <?php $this->load->view('layouts/_alert') ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Checkout berhasil
+                </div>
+                <div class="card-body">
+                    <h5>Nomor order: <?= $content->invoice ?></h5>
+                    <p>Terima kasih sudah berbelanja.</p>
+                    <p>Segera Lakukan Pembayaran Sebelum 24 Jam</p>
+                    <ol>
+                        <li>Jika Anda Memilih Pembayaran COD, silahkan tunggu hingga barang sampai kerumah Anda, lalu siapkan uang sesuai dengan harga Total Pembayaran yaitu : <strong>Rp.<?= number_format($content->total, 0, ',', '.') ?>,-</strong></li>
+                        <li>Jika Anda memilih metode pembayaran transfer bank silahkan transfer sejumlah <strong>Rp.<?= number_format($content->total, 0, ',', '.') ?>,-</strong> Ke nomor Virtual Account: <strong><?= $content->invoice ?></strong> a/n PT. ABC Destio Wahyu</li>
+                        <li>Jika Anda memilih metode pembayaran Alfamart atau Indomaret Silahkan datang ke kasir dan sampaikan ke kasir akan melakukan pembayaran TOKO ABC, lalu tunjukkan nomor Pembayaran <strong><?= $content->invoice ?></strong>, dan lakukan pembayaran sejumlah <strong>Rp.<?= number_format($content->total, 0, ',', '.') ?>,-</strong></li>
+                    </ol>
+                    <p>Pembayaran akan dilakukan verifikasi secara manual.</p>
+                    <p>Jika sudah silahkan kirimkan bukti transaksi di halaman konfirmasi atau bisa <a href="<?= base_url("myorder/detail/$content->invoice") ?>">klik disini</a></p>
+                    <strong>*Anda tidak perlu mengirim bukti transaksi jika memilih metode pembayaran COD</strong><br><br>
+                    <a href="<?= base_url('home') ?>" class="btn btn-primary"><i class="fas fa-angle-left"></i> Kembali</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
