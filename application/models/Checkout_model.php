be1ab470a695eab156eebe7f3e4cf036eb088c10
destiowahyu <destiowahyu@gmail.com>
2022-07-10 21:48:23 +0700

    commit pertama

---
+++ application/models/Checkout_model.php
@@ -0,0 +1,50 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Checkout_model extends MY_Model
+{
+    public $table = 'orders';   // Ini akan diubah2 di controller Checkout
+
+    public function getDefaultValues()
+    {
+        return [
+            'name'      => '',
+            'address'   => '',
+            'phone'     => '',
+            'status'    => '',
+            'pembayaran' => ''
+        ];
+    }
+
+    public function getValidationRules()
+    {
+        $validationRules = [
+            [
+                'field' => 'name',
+                'label' => 'Nama',
+                'rules' => 'trim|required'
+            ],
+            [
+                'field' => 'address',
+                'label' => 'Alamat',
+                'rules' => 'trim|required'
+            ],
+            [
+                'field' => 'phone',
+                'label' => 'Telepon',
+                'rules' => 'trim|required|max_length[15]'
+            ],
+            [
+                'field' => 'pembayaran',
+                'label' => 'Metode Pembayaran',
+                'rules' => 'trim|required'
+            ]
+        ];
+
+        return $validationRules;
+    }
+}
+
+
+/* End of file Checkout_model.php */
